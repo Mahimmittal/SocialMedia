32ade89541dc3712cea93c8a6a172fac919e3044
Mahim Mittal <mahim.mittal2001@gmail.com>
2022-04-16 23:11:21 +0530

    Added signin signup feature

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv=require("dotenv");
 const morgan = require("morgan");
 const helmet = require("helmet");
-
+const cors= require('cors');
 const app=express();
 const userRoute=require("./routes/users");
 const authRoute=require("./routes/auth");
@@ -12,16 +12,23 @@ dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL,{
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  
+},(err)=>{
+  if(err)
+  console.log(err);
+  console.log("Connected to mongoDB Server");
 });
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 app.use("/api/users", userRoute);
 app.use("/api/auth",authRoute);
 app.use("/api/posts",postRoute);
+
 app.get("/",(req,res)=>{
   res.send("Welcome to homepage");
 })
@@ -30,5 +37,5 @@ app.get("/",(req,res)=>{
 
 
 app.listen(8000,()=>{
-  console.log('Backend server is runningdsfs');
+  console.log('Backend server is running on port 8000');
 })
--- api/routes/users.js
+++ api/routes/users.js
@@ -25,7 +25,7 @@ router.put("/:id", async (req, res) => {
     return res.status(403).json("You can update only your account");
   }
 });
-// delte user
+// delete user
 router.delete("/:id", async (req, res) => {
   if (req.body.userId === req.params.id || req.body.isAdmin) {
 
@@ -40,9 +40,11 @@ router.delete("/:id", async (req, res) => {
   }
 });
 // get a user
-router.get("/:id",async(req,res)=>{
+router.get("/",async(req,res)=>{
+  const userId=req.query.userId;
+  const username=req.query.username;
   try{
-    const user= await User.findById(req.params.id);
+    const user= userId? await User.findById(userId): await User.findOne({username:username});
     const {password, updatedAt, ...other} = user._doc;
     res.status(200).json(other);
   } catch(err){
